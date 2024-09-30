07ff519e2932214249dd173c013fef3d6c298a88
Truong Hai Minh <144296136+minhtruong123A@users.noreply.github.com>
2024-09-30 17:09:29 +0700

    added kebab style Response Model (Schema)

--- Helper/ResponseModel.cs
+++ Helper/ResponseModel.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Helper
 {
     public class ResponseModel<T>
     {
+        [JsonPropertyName("success")]
         public bool Success { get; set; }
+
+        [JsonPropertyName("data")]
         public T Data { get; set; }
+
+        [JsonPropertyName("error")]
         public string Error { get; set; }
+
+        [JsonPropertyName("error-code")]
         public int ErrorCode { get; set; }
 
         public ResponseModel() => Success = true;
